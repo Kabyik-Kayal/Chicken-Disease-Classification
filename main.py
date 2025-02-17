9d4a022704a790625ed8e85b1b66055932f3ee0e
Kabyik-Kayal <scientistk001@gmail.com>
2025-02-17 22:24:00 +0530

    Data Ingestion Module Implemented

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Chicken_disease_classifier import logger
+from Chicken_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Testing The logger")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} Started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> Stage {STAGE_NAME} Completed Successfully <<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
