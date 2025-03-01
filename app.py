66bec6374749f6b92394d171d6a8fb747552f286
Kabyik-Kayal <scientistk001@gmail.com>
2025-03-01 19:32:49 +0530

    app updated to run on all address for EC2

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
